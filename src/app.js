fbd8c2b490e06255e744445c867dc80524911b5b
Hanzala Naseer <hanzalanaseer56@gmail.com>
2026-07-31 12:59:43 +0500

    refactor: organize application structure

--- src/app.js
+++ src/app.js
@@ -3,15 +3,10 @@ const indexRoutes = require("./routes/index.routes");
 
 const app = express();
 
-const PORT = 5001;
-
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use("/", indexRoutes);
 
-
-app.listen(PORT, () => {
-    console.log(`Observability Learning Lab running on port ${PORT}`);
-});
+module.exports = app;
